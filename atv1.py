b9683ad948d981951ee908b6845cd1e889931486
millasilva-lang <145585070+millasilva-lang@users.noreply.github.com>
2023-09-20 12:16:00 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,15 @@
+print('Ola,mundo')
+num1=float(input('digite o primeiro numero:'))
+num2=float(input('digite o orimeiro numero:'))
+
+#realiza operaçoes
+soma=num1+num2
+subtraçao=num1+num2
+multiplicaçao=num1+num2
+divisao=num1=num2
+
+#exibe os resultados 
+print(f'soma:{soma}')
+print(f'subtraçao:{subtraçao}')
+print(f'multiplicaçao:{multiplicaçao}')
+print(f'divisao:{divisao}')
